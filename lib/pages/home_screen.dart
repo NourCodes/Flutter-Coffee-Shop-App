f23de09e56f9d32a5e0eeb13e31760d459bc8896
Nour HMW <nouralhamwi2018@gmail.com>
2024-05-07 17:44:22 +0300

    Enhanced HomeScreen with bottom navigation bar and new pages

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,17 +1,43 @@
+import 'package:coffee_shop_app/pages/cart_screen.dart';
+import 'package:coffee_shop_app/pages/shop_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List pages = [const ShopScreen(), const CartScreen()];
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("You are signed in"),
-        ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ""),
+        ],
       ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: const Icon(Icons.menu),
+      ),
+      body: pages[index],
     );
   }
 }
